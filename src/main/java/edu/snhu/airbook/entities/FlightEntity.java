aa54b86db137337a5b6bf913d97092542911db38
ptt5039 <tran.phong1991@yahoo.com>
2023-12-21 14:59:22 -0500

    update script.js

--- src/main/java/edu/snhu/airbook/entities/FlightEntity.java
+++ src/main/java/edu/snhu/airbook/entities/FlightEntity.java
@@ -1,7 +1,6 @@
 package edu.snhu.airbook.entities;
 
 import java.sql.Time;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,78 +1,152 @@
-var searchForm = document.getElementById("flightSearchForm")
+var departureDateFieldInput = document.getElementById("departDate");
+var departureDateText = document.getElementById("departureDateText");
+var returnDateFieldInput = document.getElementById("returnDate");
+var returnDateText = document.getElementById("returnDateText");
+var searchForm = document.getElementById("flightSearchForm");
+var oneWayButton = document.getElementById("oneWay");
+var roundTripButton = document.getElementById("roundTrip");
+var departureAirportInput = document.getElementById("departureInput");
+var departureAirportSelect = document.getElementById("departureAirports");
+var departureLocationText = document.getElementById("departLocationText");
+var arrivalAirportInput = document.getElementById("arrivalInput");
+var arrivalAirportSelect = document.getElementById("arrivalAirports");
+var arrivalLocationText = document.getElementById("arriveLocationText");
+var dtToday = new Date();
+
+//Search form handle validate check
 searchForm.addEventListener("submit", event => {
     if (!searchForm.checkValidity()) {
       event.preventDefault()
       event.stopPropagation()
     }
     searchForm.classList.add('was-validated')
-  }, false)
+}, false)
 
 //Disable return date picker when One-way is select
-document.getElementById("oneWay").addEventListener("click", tripSelector);
-document.getElementById("roundTrip").addEventListener("click", tripSelector);
+oneWayButton.addEventListener("click", tripSelector);
+roundTripButton.addEventListener("click", tripSelector);
 function tripSelector(e) {
     if(e.target.id == "oneWay") {
-        document.getElementById("returnDate").disabled = true;
-        document.getElementById("roundTrip").checked = false;
+        returnDateFieldInput.disabled = true;
+        roundTripButton.checked = false;
+        returnDateText.innerText = "";
+        returnDateFieldInput.value = ""
     } else {
-        document.getElementById("returnDate").disabled = false;
-        document.getElementById("oneWay").checked = false;
+        returnDateFieldInput.disabled = false;
+        oneWayButton.checked = false;
     }
 }
 
 //Show departure airport name when airport IATA is selected
-document.getElementById("departureInput").addEventListener("input", onDepartureInput);
+departureAirportInput.addEventListener("input", onDepartureInput);
+departureAirportInput.addEventListener("blur", onDepartureInputBlur);
+
+//departure input validate with airport list
+function onDepartureInputBlur(e) {
+    var departValue = e.target.value;
+    var opts = departureAirportSelect.childNodes;
+    if (Object.values(opts).filter(opt => opt.value === departValue).length === 0) {
+        departureAirportInput.setAttribute("aria-invalid", "true");
+        departureAirportInput.setCustomValidity("Enter a valid departure airport.")
+    } else {
+        departureAirportInput.removeAttribute("aria-invalid")
+        departureAirportInput.setCustomValidity("")
+    }
+}
+
+//Show selected arrival airport name when airport IATA is selected
 function onDepartureInput(e) {
     var departValue = e.target.value;
-    var opts = document.getElementById("departureAirports").childNodes;
+    var opts = departureAirportSelect.childNodes;
     for (var i = 0; i < opts.length; i++) {
         if (opts[i].value === departValue) {
-            document.getElementById("departLocationText").innerText = opts[i].text;
+            departureLocationText.innerText = opts[i].text;
             break;
         } else {
-            document.getElementById("departLocationText").innerText = ""
+            departureLocationText.innerText = ""
         }
     }
 }
+
+
+arrivalAirportInput.addEventListener("input", onArrivalInput);
+arrivalAirportInput.addEventListener("blur", onArrivalInputBlur);
 
 //Show arrival airport name when airport IATA is selected
-document.getElementById("arrivalInput").addEventListener("input", onArrivalInput);
 function onArrivalInput(e) {
     var arrivalValue = e.target.value;
-    var opts = document.getElementById("arrivalAirports").childNodes;
+    var opts = arrivalAirportSelect.childNodes;
     for (var i = 0; i < opts.length; i++) {
         if (opts[i].value === arrivalValue) {
-            document.getElementById("arriveLocationText").innerText = opts[i].text;
+            arrivalLocationText.innerText = opts[i].text;
             break;
         } else {
-            document.getElementById("arriveLocationText").innerText = ""
+            arrivalLocationText.innerText = ""
         }
     }
 }
 
-document.getElementById("departDate").addEventListener("input", onDepartureDateInput);
-var today = new Date()
-document.getElementById("departDate").valueAsDate = today;
-document.getElementById("departureDateText").innerText = today.toUTCString().substring(0, today.toUTCString().length - 13);
+//arrival input validate with airport list
+function onArrivalInputBlur(e) {
+    var arrivalValue = e.target.value;
+    var opts = arrivalAirportSelect.childNodes;
+    if (Object.values(opts).filter(opt => opt.value === arrivalValue).length === 0) {
+        arrivalAirportInput.setAttribute("aria-invalid", "true");
+        arrivalAirportInput.setCustomValidity("Enter a valid arrival airport.")
+    } else {
+        arrivalAirportInput.removeAttribute("aria-invalid")
+        arrivalAirportInput.setCustomValidity("")
+    }
+}
+
+//initial date inputs
+departureDateFieldInput.addEventListener("input", onDepartureDateInput);
+departureDateFieldInput.valueAsDate = dtToday;
+departureDateText.innerText = getUTCFormattedDateString(dtToday);
+departureDateFieldInput.setAttribute("min", getISOFormattedDateString(dtToday, 0));
+returnDateFieldInput.setAttribute("min", getISOFormattedDateString(dtToday, 0));
+
+//departure date on input
 function onDepartureDateInput(e) {
     var dateValue = e.target.value;
     var date = new Date(dateValue);
-    document.getElementById("departureDateText").innerText = date.toUTCString().replace(" 00:00:00 GMT", "");
+    var departureDateText = getUTCFormattedDateString(date)
+    departureDateText.innerText = departureDateText;
+
+    returnDateFieldInput.setAttribute("min",  getISOFormattedDateString(date, 1));
 }
 
+returnDateFieldInput.addEventListener("input", onReturnDateInput);
 
-document.getElementById("returnDate").addEventListener("input", onReturnDateInput);
+//return date on input
 function onReturnDateInput(e) {
     var dateValue = e.target.value;
-    var date = new Date(dateValue);
-    var departureDate = document.getElementById("departDate").value;
+    var returnDate = new Date(dateValue);
+    var departureDateValue = departureDateFieldInput.value;
 
-    if(new Date(departureDate) < date){
-        document.getElementById("returnDateText").innerText = date.toUTCString().replace(" 00:00:00 GMT", "");
-    } else {
-        document.getElementById("returnDateText").innerText = "";
-//        document.getElementById("returnDate").setCustomValidity("Return date must be after depart date!")
+    if(new Date(departureDateValue) <= returnDate){
+        returnDateText.innerText = getUTCFormattedDateString(returnDate);
     }
+}
+
+//Convert Date to UTC E, dd MMM yyyy format
+function getUTCFormattedDateString(date) {
+    return date.toUTCString().substring(0, date.toUTCString().length - 13);
+}
+
+//Convert Date to ISO yyyy-MM-dd format with day offset
+function getISOFormattedDateString(date, offset) {
+    var month = date.getMonth() + 1;
+    var day = date.getDate() + offset;
+    var year = date.getFullYear();
+    if(month < 10) {
+        month = '0' + month.toString();
+    }
+    if(day < 10){
+        day = '0' + day.toString();
+    }
+    var minDate = year + '-' + month + '-' + day;
+    return minDate;
 }
 
 
